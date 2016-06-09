1ab1669acc5b4881e7dea065ba2fa4ddb300bc0c
Attila Glück <attilag@protonmail.ch>
2016-06-09 11:26:40 +0200

    initial commit

---
+++ startup.cmd
@@ -0,0 +1,15 @@
+@echo off
+
+call bin\config.cmd
+
+REM clean up the previous session if necessary
+if exist %DEV_DRIVE%\nul subst /D %DEV_DRIVE%
+
+REM create the development drive
+subst %DEV_DRIVE% %ROOT_PATH%
+
+REM prepare the env if this is the first start
+if not exist %HOME% md %HOME%
+if not exist %TMP% md %TMP%
+
+%DEV_DRIVE%
