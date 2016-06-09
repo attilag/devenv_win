fe9698b146b5e70510fa1661b3ab9fda7d262cae
Attila Glück <attilag@protonmail.ch>
2016-06-09 15:41:22 +0200

    softlinks works on windows only if you have admin rights

---
+++ setup-vim.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+set VIM_BASE_PATH=%HOME%\.vim
+
+xcopy /Q /Y %VIM_BASE_PATH%\_vimrc %HOME%\
+if not exist %HOME%\vimfiles\nul md %HOME%\vimfiles
+xcopy /Q /Y /E %VIM_BASE_PATH%\vimfiles %HOME%\vimfiles\
