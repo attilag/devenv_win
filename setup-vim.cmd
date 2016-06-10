ee855fadaac22e0a089ca2d67cbc217cb0b80383
Attila Glück <attilag@protonmail.ch>
2016-06-10 13:15:21 +0200

    copy snippets into the home directory

--- setup-vim.cmd
+++ setup-vim.cmd
@@ -5,3 +5,4 @@ set VIM_BASE_PATH=%HOME%\.vim
 xcopy /Q /Y %VIM_BASE_PATH%\_vimrc %HOME%\
 if not exist %HOME%\vimfiles\nul md %HOME%\vimfiles
 xcopy /Q /Y /E %VIM_BASE_PATH%\vimfiles %HOME%\vimfiles\
+xcopy /Q /Y /E %VIM_BASE_PATH%\snippet %HOME%\vimfiles\snippet\
